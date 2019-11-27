7f6ddda24db25e6daf140b2bb1b288fb408a2228
sergio <sergiohidalgocaceres@gmail.com>
2019-11-26 20:44:50 -0500

    update

---
+++ modulo01/clase03/form/guardar.php
@@ -0,0 +1,16 @@
+// recojos los datos del formulario
+valido si son correecto
+
+// si son correctos
+redirect(success.html)
+<html>
+	<h1>Gacias por registrar</h1>
+	<a href="">Regresar</a>
+</html>
+
+//no son correctos
+redirect(error.html)
+<html>
+	<h1>Datos equivocados</h1>
+	<a href="">Regresar</a>
+</html>
