7bb397cdfa3220533dd0fbeacb17e77d3886e3c7
Alonso Perez <nuevocorreo17@hotmail.com>
2020-02-11 21:40:36 -0500

    ejemplo scroll

--- modulo03/clase04/modal/js/main.js
+++ modulo03/clase04/modal/js/main.js
@@ -92,3 +92,30 @@ $(".op-tab").click(function(){
 		}
 	}*/
 })
+
+/*
+$(window).load(function(){
+
+})
+
+$(document).ready(function(){
+
+})
+*/
+$(window).scroll(function(){
+
+
+	let top=$(window).scrollTop();
+	console.log(top);
+	if(top>200){
+		$("header").addClass("header-fixed");
+		//console.log("registra el scroll")
+	}
+	else{
+		$("header").removeClass("header-fixed");
+	}
+
+	if(top>300){
+		$(".item").addClass("show-item");
+	}	
+})
