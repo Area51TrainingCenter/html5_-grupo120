2cd8e8e96d6fda11b6f65919e8bf7654b753c713
jtorriccelli1986@gmail.com <jtorriccelli1986@gmail.com>
2020-02-24 21:22:18 -0500

    update

---
+++ modulo03/clase07/for/js/main.js
@@ -0,0 +1,15 @@
+for(var i=0;i<10;i++){
+
+	console.log("el valor de i es igual= "+i);
+}
+
+
+var nombres=["Juan","Diego","Luis"]
+
+var limite=nombres.length // cantidad de elementos;
+console.log(limite);
+
+for(var i=0;i<limite;i++){
+	console.log(nombres[i])
+}
+
