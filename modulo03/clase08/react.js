7c0cd9263c44925c28017a87bbdff426974c27bf
Alonso Perez <nuevocorreo17@hotmail.com>
2020-02-25 22:02:08 -0500

    update

---
+++ modulo03/clase08/react.js
@@ -0,0 +1,18 @@
+function header_react(){
+	constructor(){
+		title="jc"
+		this.state=1;
+	}
+	return (
+		<header>
+			<h1>{title}</h1>
+			<nav></nav>
+		</header>
+		)
+
+
+	cambiarestad(){
+		title="Diego"
+		this.state=2;
+	}
+}
